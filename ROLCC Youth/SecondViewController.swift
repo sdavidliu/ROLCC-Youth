7b9a11e765c1f679f1c757622329f8b5bb7a6b8f
sdavidliu <sdavidliu@gmail.com>
2016-06-22 17:36:53 -0700

    Initial Commit

---
+++ ROLCC Youth/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ROLCC Youth
+//
+//  Created by Jimy Liu Mini on 6/22/16.
+//  Copyright © 2016 Dave&Joe. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
